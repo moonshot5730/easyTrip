f0913260a5b05766605a59c1a26ffa9b1a1dcefa
문현준 <moonshot5730@gmail.com>
2025-06-18 23:28:50 +0900

    재귀적으로 호출되고 있는 구조 개선 필요.. -> 사용자 경험을 유도할 수 있는 그래프 구조 세팅 필요

--- app/cognitive_service/agent_langgraph/agent_condition.py
+++ app/cognitive_service/agent_langgraph/agent_condition.py
@@ -4,9 +4,9 @@ from app.cognitive_service.agent_langgraph.agent_state import AgentState
 def should_go_to_router(state: AgentState) -> str:
     # 정보가 다 채워졌는지 확인
     required_fields = [
-        state.want_travel_city,
-        state.travel_schedule,
-        state.travel_style
+        state.get("want_travel_city"),
+        state.get("travel_schedule"),
+        state.get("travel_style")
     ]
     # 최소 하나라도 비어 있으면 계속 대화
     if any(field is None for field in required_fields):
