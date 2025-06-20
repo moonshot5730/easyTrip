8829e67c0b3a20c604aa32f5e58a1ed1f5594d4f
문현준 <moonshot5730@gmail.com>
2025-06-20 17:12:51 +0900

    상태를 받아서 보여주기 위한 API 추가

--- frontend/client_constant/trip_api_constant.py
+++ frontend/client_constant/trip_api_constant.py
@@ -1,2 +1,5 @@
-TRAVEL_API_URL = "http://localhost:8000/trip/plan/test/stream"
+TRAVEL_API_URL = "http://localhost:8000//trip/plan/astream-event"
+LANG_STATE_URL = "http://localhost:8000/trip/plan/langgraph/state"
+
 START_MESSAGE = {"role": "assistant", "content": "안녕하세요! 무엇을 도와드릴까요?"}
+
